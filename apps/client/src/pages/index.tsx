9fd7c64cc5810e2599dd9ec7002829fe5c04923d
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-08-12 05:06:06 +0700

    feat(client): add index page

---
+++ apps/client/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Container from "../components/Container";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Toolbar from "../components/Toolbar";
+
+function Home() {
+  return (
+    <Container>
+      <Header />
+      <Toolbar />
+      <Footer />
+    </Container>
+  );
+}
+
+export default Home;
