81d3644fd6ec33677bb0fb141a2783c3c78e46f9
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-08-15 07:46:47 +0700

    feat(server): add server.ts

---
+++ apps/server/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from "fastify";
+
+const server = Fastify({
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
+      },
+    },
+  },
+});
+
+export default server;
