4e3d6bf1b1f3d25543601e771c58b4aa12d23e8e
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-08-19 08:42:38 +0700

    docs(server): add jsdoc for `getTokenFromHeader`

--- apps/server/src/helpers/getTokenFromHeader.ts
+++ apps/server/src/helpers/getTokenFromHeader.ts
@@ -1,3 +1,14 @@
+/**
+ * Get token from an authorization header
+ *
+ * @param input
+ * @returns token
+ *
+ * @example
+ * const authorization = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6ImJlYmVrIGdvcmVuZyBlbmFrIHJhc2FueWEiLCJpYXQiOjE1MTYyMzkwMjJ9.KkcoWh77Q5GBaQgD2K8WXXy82lIplgJV4T8MtYrXs6o"
+ * const token = getTokenFromHeader(authorization);
+ *
+ */
 function getTokenFromHeader(input: string) {
   try {
     const token = input.split(" ")[1];
