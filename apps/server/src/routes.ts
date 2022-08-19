8333113c1b5e95bcaee29bba3ece44f736b7a073
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-08-19 10:01:45 +0700

    feat(server): add post vote routes

--- apps/server/src/routes.ts
+++ apps/server/src/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance, FastifyPluginOptions } from "fastify";
 import postRoute from "@/routes/post.route";
 import userRoutes from "@/routes/user.route";
 import authRoutes from "@/routes/auth.route";
+import postVoteRoute from "@/routes/post-vote.route";
 
 async function routes(
   fastify: FastifyInstance,
@@ -11,6 +12,7 @@ async function routes(
   fastify.register(authRoutes, { prefix: "/auth" });
   fastify.register(userRoutes, { prefix: "/users" });
   fastify.register(postRoute, { prefix: "/posts" });
+  fastify.register(postVoteRoute, { prefix: "/post-votes" });
 
   return next();
 }
