8845c9ed7f5200457df630984fca0c2b61ed0236
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-08-22 04:35:58 +0700

    feat(server): add handler

--- apps/server/src/routes/reply-vote.route.ts
+++ apps/server/src/routes/reply-vote.route.ts
@@ -1,3 +1,4 @@
+import replyVoteController from "@/controllers/reply-vote.controller";
 import { postVotePutSchema } from "@/schemas/post-vote.schema";
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
 
@@ -9,7 +10,11 @@ async function replyVoteRoute(
   /**
    * PUT /reply-votes/:postId
    */
-  fastify.put("/:postId", { schema: postVotePutSchema }, () => null);
+  fastify.put(
+    "/:postId",
+    { schema: postVotePutSchema },
+    replyVoteController.put
+  );
 
   return next();
 }
