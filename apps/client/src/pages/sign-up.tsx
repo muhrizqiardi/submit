9ea23ab80164737d539096dbac7cc91110489232
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-09-04 17:16:47 +0700

    feat(client): add sign up page

---
+++ apps/client/src/pages/sign-up.tsx
@@ -0,0 +1,16 @@
+import SignUpForm from "@/components/SignUpForm";
+import Container from "@/components/Container";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+function Home() {
+  return (
+    <Container>
+      <Header />
+      <SignUpForm />
+      <Footer />
+    </Container>
+  );
+}
+
+export default Home;
