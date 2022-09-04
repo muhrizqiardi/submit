ed43cb367bd4195d3f390ab91040a2603c72ffe5
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-09-04 17:16:52 +0700

    feat(client): add sign in page

---
+++ apps/client/src/pages/sign-in.tsx
@@ -0,0 +1,16 @@
+import Container from "@/components/Container";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import SignInForm from "@/components/SignInForm";
+
+function Home() {
+  return (
+    <Container>
+      <Header />
+      <SignInForm />
+      <Footer />
+    </Container>
+  );
+}
+
+export default Home;
