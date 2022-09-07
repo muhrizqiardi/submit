bfaed73c5b7653eaea05085ec1b21acb6a2d410e
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-09-07 08:55:59 +0700

    fix(client): remove unused header

--- apps/client/src/helpers/axiosInstance.ts
+++ apps/client/src/helpers/axiosInstance.ts
@@ -1,24 +1,17 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
-let defaultAxiosRequestConfig: AxiosRequestConfig = {};
-
-if (process !== undefined) {
-  defaultAxiosRequestConfig.baseURL = process?.env?.API_BASE_URL;
-} else {
-  defaultAxiosRequestConfig.baseURL = "https://localhost:9000";
-}
+let defaultAxiosRequestConfig: AxiosRequestConfig = {
+  baseURL: process?.env?.API_BASE_URL ?? "http://localhost:9000",
+};
 
 const axiosInstance = <D = any>(
   axiosRequestConfig: AxiosRequestConfig<D> = defaultAxiosRequestConfig
 ): AxiosInstance => {
-  let token = window.localStorage.getItem("token");
-
-  if (token === null) return axios.create(axiosRequestConfig);
 
   return axios.create({
     ...axiosRequestConfig,
     headers: {
-      authorization: `Bearer ${token}`,
+      "Access-Control-Allow-Origin": "*",
     },
   });
 };
