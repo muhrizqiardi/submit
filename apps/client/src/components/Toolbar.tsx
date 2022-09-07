706ef36ec5dc34ba9a9eabdfec06dfd93cb770ed
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2022-09-07 09:33:54 +0700

    fix(client): fix sort menu styling

--- apps/client/src/components/Toolbar.tsx
+++ apps/client/src/components/Toolbar.tsx
@@ -3,7 +3,7 @@ function Toolbar() {
     <div className="mb-4 p-2 rounded-lg bg-custom-light text-custom-dark leading-none flex items-center justify-start gap-3">
       <div className="inline-flex items-center gap-2">
         <span>sort by:</span>
-        <select className="p-2 bg-gray-300 hover:bg-gray-400 rounded-lg">
+        <select className="">
           <option>New</option>
         </select>
       </div>
